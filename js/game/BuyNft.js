bbb1d02e1ff763fd41929694f9bf4127a1f9e610
KillAlone <killalonecrypto@gmail.com>
2022-02-13 03:33:51 -0300

    First Update

---
+++ js/game/BuyNft.js
@@ -0,0 +1,62 @@
+const serverUrl = "https://cpovkemuvrve.usemoralis.com:2053/server";
+const appId = "KgXX7vY0zeT0HYeQSo5KgQCQesReOWkgnoJAWJq3";
+Moralis.start({ serverUrl, appId });
+
+let user = Moralis.User.current();
+
+async function buyNft ()
+{
+    let buyNft   = await fetch('http://localhost:3000/buy/' + user.get("ethAddress"))
+
+    let response = await buyNft.json()
+
+    if (response.code === 200) 
+    {
+        let divNftContainer = document.getElementById('nft_container')
+        divNftContainer.style.display = 'none'
+
+        let divNftContainerWin = document.getElementById('nft_container_win')
+
+        divNftContainerWin.style.display = 'flex'
+
+        let life_nft = document.getElementById('life_nft').innerHTML = `Life ${response.life}`
+    }
+
+
+
+}
+
+async function showNft()
+{
+    let buyNft   = await fetch('http://localhost:3000/show/' + user.get("ethAddress"))
+
+    let response = await buyNft.json()
+
+    if (response.code === 200) 
+    {
+        let divNftContainer = document.getElementById('nft_container')
+        divNftContainer.style.display = 'none'
+
+        let divNftContainerWin = document.getElementById('nft_container_win')
+
+        divNftContainerWin.style.display = 'flex'
+
+        let life_nft = document.getElementById('life_nft')
+        
+        life_nft.innerHTML = `Life ${response.life}`
+
+        console.log(response)
+    }
+    else 
+    {
+        let divNftContainer = document.getElementById('nft_container')
+        divNftContainer.style.display = 'flex'
+
+        let divNftContainerWin = document.getElementById('nft_container_win')
+
+        divNftContainerWin.style.display = 'none'
+    }
+
+}
+
+showNft()
---
+++ js/game/Login.js
@@ -0,0 +1,91 @@
+const serverUrl = "https://cpovkemuvrve.usemoralis.com:2053/server";
+const appId = "KgXX7vY0zeT0HYeQSo5KgQCQesReOWkgnoJAWJq3";
+Moralis.start({ serverUrl, appId });
+
+let user = Moralis.User.current();
+
+let loginBtn  = document.getElementById('login')
+let logoutBtn = document.getElementById('logout')
+
+
+async function login() {
+    if (!user) 
+    {
+      user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+        .then(function (user) 
+        {
+          registerLogin(user.get("ethAddress"))
+        })
+        .catch(function (error) 
+        {
+          console.log(error);
+        });
+    }
+    else 
+    {
+        await showUser ()
+        console.log(user.get("ethAddress"))
+    }
+}
+
+async function registerLogin(user) {
+    let sendLogin = await fetch('http://localhost:3000/register' + '/' + user)
+
+    let response  = await sendLogin.json()
+
+    console.log(response)
+
+    if (response.message === 1) {
+        window.location.reload()
+    }
+}
+
+let count = 0
+
+async function showUser () {
+    let reciveLogin = await fetch('http://localhost:3000/user' + '/' +user.get("ethAddress"))
+
+    let dataLogin   = await reciveLogin.json()
+
+    let addressFront = document.getElementById('address')
+    let balanceFront = document.getElementById('balance')
+    let token_acess  = document.getElementById('token_acess')
+
+    
+
+    console.log(dataLogin)
+
+    let show_token   = document.getElementById('show_token')
+
+    show_token.addEventListener('click', () => {
+        
+        count = count + 1
+
+        if (count === 1) {
+            token_acess.innerHTML = `Token: ${dataLogin.showUserDB[0].token_acess}`
+        }
+        else if (count === 2) {
+            count = 0
+            token_acess.innerHTML = 'Token: *****'
+        }
+    })
+
+    addressFront.innerHTML = dataLogin.showUserDB[0].address
+    balanceFront.innerHTML = `Balance: ${dataLogin.showUserDB[0].balance_usdt}`
+
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('logout').style.display = 'inline';
+
+
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  window.location.reload()
+}
+
+login()
+
+document.getElementById('logout').style.display = 'none';
+document.getElementById("login").onclick = login
+document.getElementById("logout").onclick = logOut
