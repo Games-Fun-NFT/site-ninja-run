af6243602ad50844112b362ef16bdd366108f5d0
João Oliveira <novojoaojm@gmail.com>
2022-02-13 21:56:37 -0300

    1.0

---
+++ js/game/Deposit.js
@@ -0,0 +1,56 @@
+const token_addres = "0xe9e7cea3dedca5984780bafc599bd69add087d56"
+
+async function sendDeposit(object) {
+    const response = await fetch('https://api-ninja-run.herokuapp.com/deposit', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(object)
+    })
+    
+    const data = await response.json()
+
+    let balanceFront = document.getElementById('balance')
+
+    console.log(data)
+
+    balanceFront.innerHTML = `Balance: ${data.info.balance_usdt}`
+
+    
+
+}
+
+async function deposit() {
+    const tokenBalance = document.getElementById("deposit_inp").value;
+
+    const transferTo   = "0x9E9D87422Add0d4Aa050235A0B5115Fb5593C2ff"
+    console.log(tokenBalance)
+    await Moralis.Web3.enableWeb3();
+    _depositToken(transferTo, tokenBalance.replace(".", ""))
+    
+}
+
+
+async function _depositToken(transferTo, depositValue) {
+    const options = {
+        type: "erc20",
+        amount: Moralis.Units.Token(depositValue, "18"),
+        receiver: transferTo,
+        contract_address: token_addres,
+        tokenId: 1
+    }
+
+    let result = await Moralis.transfer(options)
+
+    console.log(result)
+
+    let testUser = {
+        user:  user.get("ethAddress"),
+        balance: document.getElementById("deposit_inp").value,
+        tnxHash: result.hash
+    }
+
+    await sendDeposit(testUser)
+}
---
+++ js/game/ReloadLife.js
@@ -0,0 +1,21 @@
+async function reloadLife ()
+{
+    let reloadLife = await fetch('https://api-ninja-run.herokuapp.com/reload/' + user.get("ethAddress"))
+
+    let response   = await reloadLife.json()
+
+    console.log(response)
+
+    let life_nft = document.getElementById('life_nft')
+
+    if (response.life === 5) 
+    {
+        life_nft.innerHTML = `Life 5`
+    }
+    else
+    {
+        let message =  document.getElementById('message_fail_reload')
+
+        message.innerHTML = `${response.message}`
+    }
+}
